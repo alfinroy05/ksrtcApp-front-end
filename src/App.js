49ed75aeee2a442ff998f648ce2d2f5580c9e0c5
alfinroy05 <alfinroy2005@gmail.com>
2024-04-08 21:39:15 +0530

    add,delete,search completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import AddBus from './ksrtcApp/AddBus';
+import DeleteBus from './ksrtcApp/DeleteBus';
+import SearchBus from './ksrtcApp/SearchBus';
 
 function App() {
   return (
     <div>
-      
+      <AddBus/>
+      <DeleteBus/>
+      <SearchBus/>
     </div>
   );
 }
---
+++ src/ksrtcApp/AddBus.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const AddBus = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">BUS ID</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">NUMBER</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">ROUTE</label>
+                           <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">DRIVER NAME</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">DRIVER NUMBER</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success">ADD</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddBus
